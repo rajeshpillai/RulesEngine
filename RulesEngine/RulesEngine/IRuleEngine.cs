fa5287dc98b6f0013063e990dd6ad8a7fd282e82
Rajesh Pillai <pillai.rajesh@gmail.com>
2014-07-14 15:50:43 +0530

    Fixed namespacing.

--- RulesEngine/RulesEngine/IRuleEngine.cs
+++ RulesEngine/RulesEngine/IRuleEngine.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RulesEngine.Rules;
 
-namespace RulesEngine.Rules
+namespace RulesEngine
 {
     public interface IRuleEngine<T>
     {
